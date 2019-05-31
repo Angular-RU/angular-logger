659edc0f47abf78cd4cf340dd6c9a404fff69345
e.zbarskaya <e.zbarskaya@tinkoff.ru>
2019-05-31 12:33:32 +0300

    refactor: fix type after code review

--- projects/logger/src/lib/decorators/timer.decorator.ts
+++ projects/logger/src/lib/decorators/timer.decorator.ts
@@ -1,12 +1,12 @@
 import { LoggerLevel } from '../logger.config';
-import { TimerInfo, TimerLevels } from '../logger.interfaces';
+import { Fn, TimerInfo, TimerLevels } from '../logger.interfaces';
 import { LoggerModule } from '../logger.module';
 
 export function Timer(title: string, level: TimerLevels = LoggerLevel.DEBUG, isMillisecond: boolean = true): any {
     return (_target: any, _key: string, descriptor: PropertyDescriptor): PropertyDescriptor => {
         let result: any;
-        const method: any = descriptor.value;
-        descriptor.value = function(...args: any[]): any {
+        const method: Fn = descriptor.value;
+        descriptor.value = function(...args: any[]): PropertyDescriptor {
             const info: TimerInfo | null = LoggerModule.logger.startTime(title, level);
             result = method.apply(this, args);
             LoggerModule.logger.endTime(info, level, isMillisecond);
