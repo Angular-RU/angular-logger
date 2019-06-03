5e7972ef99c7fe1788f628b3f3c13a694802bdfa
e.zbarskaya <e.zbarskaya@tinkoff.ru>
2019-06-03 18:04:24 +0300

    small fix

--- projects/logger/src/lib/decorators/logger.decorator.ts
+++ projects/logger/src/lib/decorators/logger.decorator.ts
@@ -2,7 +2,6 @@ import { Type } from '@angular/core';
 
 import { LoggerModule } from '../logger.module';
 import { LoggerService } from '../logger.service';
-import { Type } from '@angular/core';
 
 export function Logger(): PropertyDecorator {
     return (target: Type<unknown>, propertyName: string): void => {
