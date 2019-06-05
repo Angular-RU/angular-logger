02bf0f23a24ea569fad0244a5219e86734860a7c
e.zbarskaya <e.zbarskaya@tinkoff.ru>
2019-06-05 10:43:17 +0300

    refactor: fix types

--- projects/logger/src/lib/logger.service.ts
+++ projects/logger/src/lib/logger.service.ts
@@ -9,10 +9,13 @@ import { JsonFactory } from './services/json-factory.service';
 import { ClipboardFactory } from './services/clipboard-factory.service';
 import { TimerFactory } from './services/timer-factory.service';
 import { LoggerOptionsImpl } from './logger.options';
+import { autobind } from './decorators/autobind.decorator';
 
+@autobind
 @Injectable()
 export class LoggerService {
     private readonly DEFAULT_DEPTH: number = 2;
+
     constructor(
         public readonly clipboard: ClipboardFactory,
         public readonly cssFactory: CssFactory,
